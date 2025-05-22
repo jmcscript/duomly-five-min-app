342627a5b3caf09a001caa5672af3f793177bf25
James McDonald <jmcscript@gmail.com>
2025-05-22 17:25:18 +0000

    feat: add Boostrap and scaffold JSON data

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,21 @@ class App extends Component {
     const posts = this.state.posts;
 
     return (
-      <>
-        <h1>Duomly 5-Min React App</h1>
-        <p>We have {posts.length} posts</p>
-      </>
+      <div className="container">
+        <div class="jumbotron">
+          <h1 class="display-4">Blog posts</h1>
+        </div>
+        {posts.map((post) => (
+          <div className="card" key={post.id}>
+            <div className="card-header">
+              <strong>ID:</strong> #{post.id} | <strong>Title:</strong> {post.title}
+            </div>
+            <div className="card-body">
+              <p className="card-text">{post.body}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     );
   }
 }
